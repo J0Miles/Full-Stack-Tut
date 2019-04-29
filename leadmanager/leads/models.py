db78d7b04a3bb4ce92357fc50903d9c453b1d592
NeutralDread <jordanamiles@hotmail.com>
2019-04-29 08:54:36 -0500

    Fixed minor Error

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Lead(models.Model):
-    name = model.CharField(max_length=100)
-    email = model.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
