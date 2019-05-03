3527129f3e0716fd6a99e0089e826be815ccfb82
NeutralDread <jordanamiles@hotmail.com>
2019-05-03 00:15:14 -0500

    Added Header styling

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './layout/Header';
+
 class App extends Component {
   render() {
-    return <h1>React App</h1>
+    return (
+      <Header />
+    )
   }
 }
 
-ReactDOM.render(<App></App>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
