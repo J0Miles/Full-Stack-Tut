fc8c457cdd7b62da2e405d37185daf7a2d9a23c1
NeutralDread <jordanamiles@hotmail.com>
2019-05-03 19:35:16 -0500

    Starting store

---
+++ leadmanager/frontend/src/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware} from 'redux';
+import { composeWithDevtools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const intitialState = {};
+
+const middleware= [thunk];
+
+const store = createStore();
