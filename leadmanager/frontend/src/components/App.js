7162f1e99d6c7dfce3513a26e9cc70bf6c0657b0
NeutralDread <jordanamiles@hotmail.com>
2019-05-04 11:38:31 -0500

    importing store and Provider from redux

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -4,17 +4,20 @@ import ReactDOM from 'react-dom';
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
 
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component {
   render() {
     return (
+<Provider>
 <Fragment>
 <Header />
 <div className="container">
 <Dashboard />
 </div>
 </Fragment>
-
+</Provider>
     )
   }
 }
