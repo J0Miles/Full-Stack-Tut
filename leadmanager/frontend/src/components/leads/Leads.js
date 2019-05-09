baa7aa273f598f1c4f66522679ed0bb12be760de
NeutralDread <jordanamiles@hotmail.com>
2019-05-09 17:02:05 -0500

    Imported Leads to Redux backend

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getLeads } from '../../actions/leads';
@@ -8,11 +8,15 @@ static PropTypes = {
   leads: PropTypes.array.isRequired
 };
 
+componentDidMount() {
+  this.props.getLeads();
+}
+
   render () {
   return (
-    <div>
+    <Fragment>
       <h1>Leads List</h1>
-      </div>
+      </Fragment>
   );
   }
 }
@@ -21,4 +25,4 @@ const mapStateToProps = state => ({
   leads: state.leads.leads
 });
 
-export default connect(mapStateToProps)(Leads);
+export default connect(mapStateToProps, { getLeads })(Leads);
