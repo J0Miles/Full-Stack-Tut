a673fbef79fb8889e1b16f15bb3a0160d0e507f5
NeutralDread <jordanamiles@hotmail.com>
2019-05-09 17:18:24 -0500

    Added markup for Data Table

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -15,7 +15,29 @@ componentDidMount() {
   render () {
   return (
     <Fragment>
-      <h1>Leads List</h1>
+<h2>Leads</h2>
+<table className="table table-striped">
+<thead>
+<tr>
+<th>Id</th>
+  <th>Name</th>
+    <th>Email</th>
+      <th>Message</th>
+        <th />
+</tr>
+</thead>
+<tbody>
+{this.props.leads.map(lead => (
+  <tr key={lead.id}>
+<td>{lead.id}</td>
+  <td>{lead.name}</td>
+    <td>{lead.email}</td>
+      <td>{lead.message}</td>
+        <td><button className="btn btn-danger btn-sm">Delete</button></td>
+  </tr>
+))}
+</tbody>
+</table>
       </Fragment>
   );
   }
