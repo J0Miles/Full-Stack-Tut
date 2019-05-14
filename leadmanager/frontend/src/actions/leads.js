355907490f99a33b5108489fcebb6a1c6881bdab
NeutralDread <jordanamiles@hotmail.com>
2019-05-14 18:19:27 -0500

    Created Add Lead functionality

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,26 +1,42 @@
 import axios from 'axios';
 
-import { GET_LEADS, DELETE_LEAD } from './types';
+import {
+  GET_LEADS,
+  DELETE_LEAD,
+  ADD_LEAD
+} from './types';
 
 // Get Leads
 export const getLeads = () => dispatch => {
   axios.get('/api/leads/')
-  .then(res => {
-    dispatch({
-      type: GET_LEADS,
-      payload: res.data
-    });
-  }).catch(err => console.log(err));
+    .then(res => {
+      dispatch({
+        type: GET_LEADS,
+        payload: res.data
+      });
+    }).catch(err => console.log(err));
 };
 
 // Delete Lead
 export const deleteLead = (id) => dispatch => {
   axios
-  .delete(`/api/leads/${id}/`)
-  .then(res => {
-    dispatch({
-      type: DELETE_LEAD,
-      payload: id
-    });
-  }).catch(err => console.log(err));
+    .delete(`/api/leads/${id}/`)
+    .then(res => {
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id
+      });
+    }).catch(err => console.log(err));
+};
+
+// Add Lead
+export const addLeads = (lead) => dispatch => {
+  axios
+    .post('/api/leads/', lead)
+    .then(res => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
+      });
+    }).catch(err => console.log(err));
 };
--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,22 +1,31 @@
-import { GET_LEADS, DELETE_LEAD } from '../actions/types.js';
+import {
+  GET_LEADS,
+  DELETE_LEAD,
+  ADD_LEAD
+} from '../actions/types.js';
 
 const initialState = {
-  leads:[]
+  leads: []
 }
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_LEADS:
-    return {
-      ...state,
-      leads: action.payload
-    };
+      return {
+        ...state,
+        leads: action.payload
+      };
     case DELETE_LEAD:
-    return {
-      ...state,
-      leads: state.leads.filter(lead => lead.id !== action.payload)
-    }
+      return {
+        ...state,
+        leads: state.leads.filter(lead => lead.id !== action.payload)
+      };
+    case ADD_LEAD:
+      return {
+        ...state,
+        leads: [...state.leads, action.payload]
+      };
     default:
-    return state;
+      return state;
   }
 }
