bf895a066df2b79418941cc3d57208bc4febb0a4
NeutralDread <jordanamiles@hotmail.com>
2019-05-21 23:37:22 -0500

    Added Authentication to Errors alert

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import { createMessage } from './messages';
+import {
+  createMessage,
+  returnErrors
+} from './messages';
 
 import {
   GET_LEADS,
   DELETE_LEAD,
   ADD_LEAD,
-  GET_ERRORS
 } from './types';
 
 // Get Leads
@@ -16,7 +18,9 @@ export const getLeads = () => dispatch => {
         type: GET_LEADS,
         payload: res.data
       });
-    }).catch(err => console.log(err));
+    })
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
 // Delete Lead
@@ -24,7 +28,9 @@ export const deleteLead = (id) => dispatch => {
   axios
     .delete(`/api/leads/${id}/`)
     .then(res => {
-      dispatch(createMessage({ deleteLead: 'Lead Deleted'}));
+      dispatch(createMessage({
+        deleteLead: 'Lead Deleted'
+      }));
       dispatch({
         type: DELETE_LEAD,
         payload: id
@@ -37,19 +43,14 @@ export const addLead = (lead) => dispatch => {
   axios
     .post('/api/leads/', lead)
     .then(res => {
-      dispatch(createMessage({ addLead: 'Lead Added'}));
+      dispatch(createMessage({
+        addLead: 'Lead Added'
+      }));
       dispatch({
         type: ADD_LEAD,
         payload: res.data
       });
-    }).catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      }
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    })
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status)));
 };
--- leadmanager/frontend/src/actions/messages.js
+++ leadmanager/frontend/src/actions/messages.js
@@ -1,4 +1,4 @@
-import { CREATE_MESSAGE } from './types';
+import { CREATE_MESSAGE, GET_ERRORS } from './types';
 
 // CREATE MESSAGE
 export const createMessage = msg => {
