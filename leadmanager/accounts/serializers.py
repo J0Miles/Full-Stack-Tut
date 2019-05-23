6a253d09e0f42508e761567a8a2e8dafd5c2501b
NeutralDread <jordanamiles@hotmail.com>
2019-05-23 17:06:21 -0500

    Created Register API

--- leadmanager/accounts/serializers.py
+++ leadmanager/accounts/serializers.py
@@ -15,5 +15,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'password')
 extra_kwargs = {'password': {'write_only': True}}
 
+def create(self, validated_data):
+    user = User.objects.create_user(validated_data ['username'],
+    validated_data['email'], validated_data['password'])
+
+return user
 
 # Login Serializer
