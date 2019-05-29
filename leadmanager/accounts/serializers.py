f67ff7e5a6800326c60ea720cab4fa6578958741
NeutralDread <jordanamiles@hotmail.com>
2019-05-28 22:25:43 -0500

    Started Authentication through API

--- leadmanager/accounts/serializers.py
+++ leadmanager/accounts/serializers.py
@@ -22,3 +22,12 @@ def create(self, validated_data):
     return user
 
 # Login Serializer
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
