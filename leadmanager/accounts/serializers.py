ff2f73cd7656dcbb68c3c6cd4748f7abb23be296
NeutralDread <jordanamiles@hotmail.com>
2019-05-29 21:46:21 -0500

    Added authentication for user to login

--- leadmanager/accounts/serializers.py
+++ leadmanager/accounts/serializers.py
@@ -30,4 +30,4 @@ class LoginSerializer(serializers.Serializer):
         user = authenticate(**data)
         if user and user.is_active:
             return user
-        raise serializers.ValidationError("Incorrect Credentials")
+            raise serializers.ValidationError("Incorrect Credentials")
--- leadmanager/accounts/urls.py
+++ leadmanager/accounts/urls.py
@@ -5,6 +5,6 @@ from knox import views as knox_views
 urlpatterns = [
 path('api/auth', include('knox.urls')),
 path('api/auth/register', RegisterAPI.as_view()),
-path('api/auth/login', LoginAPI.as_view())
+path('api/auth/login', LoginAPI.as_view()),
 
 ]
